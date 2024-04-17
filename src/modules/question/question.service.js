96d0e80a2b5337079b68dda1762a963e37fb1d2b
Cyberverse2 <ejioforcelestine77@gmail.com>
2024-04-17 05:08:20 +0100

    removed populate in questionService

--- src/modules/question/question.service.js
+++ src/modules/question/question.service.js
@@ -4,8 +4,8 @@ export async function createQuestion(body) {
     return await Question.create(body)
 }
 
-export async function getAnswers(QuestionId) {
-    const question = await Question.findById(QuestionId).populate('questions')
+export async function getAnswers(questionId) {
+    const question = await getQuestionById(questionId)
     return question.answers
 }
 
